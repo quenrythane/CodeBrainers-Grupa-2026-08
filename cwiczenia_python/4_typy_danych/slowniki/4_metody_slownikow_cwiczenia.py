145aa0da31412647af231fbd6801015d5ab9ed93
quenrythane <artur.babinski.g@gmail.com>
2026-09-12 11:08:05 +0200

    start zadania z metodami na słownikach

---
+++ cwiczenia_python/4_typy_danych/slowniki/4_metody_slownikow_cwiczenia.py
@@ -0,0 +1,14 @@
+slownik_owocow = {
+    "klucz": "wartosc",
+    "Artur": "Ananas",
+    "Basia": "Banan",
+    "Celina": "Cytryna",
+    "Dawid": "Daktyle",
+    "Ewa": "Eszeweria"
+}
+
+
+"""
+z podanego słownika wyprintuj wszystkie żeńskie imiona
+klucze = slownik_owocow.keys()
+"""
