45580d73ebabe3444e91e27c2be168b4448058e2
quenrythane <artur.babinski.g@gmail.com>
2026-09-12 13:36:26 +0200

    omówienie ifów

---
+++ cwiczenia_python/5_elementy_jezyka/2_instrukcje_warunkowe/4_wstęp_do_ifów.py
@@ -0,0 +1,13 @@
+imie = "Kasia"
+wiek = 50
+
+
+if imie == "Kasia":
+    print("1. kod wewnątrz if. Wykona sie tylko wtedy jeżeli if True:")
+    print("2. To jest wewnątrz bloku if")
+else:
+    print("3. Kod else się wykona tylko jeżeli powyższy if gył równy False")
+    print("4. To jest wewnątrz bloku else")
+
+
+print("5. Wyokna się zawsze jest poza blokami if i else")
