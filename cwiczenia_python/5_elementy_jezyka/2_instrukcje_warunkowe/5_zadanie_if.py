502b07383085f5df0f5afa29554219bf8f8b71eb
quenrythane <artur.babinski.g@gmail.com>
2026-09-12 13:39:18 +0200

    start zadania z ifów

---
+++ cwiczenia_python/5_elementy_jezyka/2_instrukcje_warunkowe/5_zadanie_if.py
@@ -0,0 +1,9 @@
+imie = 1
+wiek = 1
+
+"""
+Sprawdź czy osoba jest kobietą pełnoletnią (18 lat).
+Jeżeli tak to wyprintuje (energetyki rządzą)
+a jeśli to nieprawda to wyprintuje (wolę jabłka)
+"""
+
