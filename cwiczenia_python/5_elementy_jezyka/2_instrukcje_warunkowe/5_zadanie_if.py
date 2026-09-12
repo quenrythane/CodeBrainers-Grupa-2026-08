6a46f9a24ef099d8a403575fae3d14a202a246a3
quenrythane <artur.babinski.g@gmail.com>
2026-09-12 13:40:48 +0200

    dodanie warunków testowych

--- cwiczenia_python/5_elementy_jezyka/2_instrukcje_warunkowe/5_zadanie_if.py
+++ cwiczenia_python/5_elementy_jezyka/2_instrukcje_warunkowe/5_zadanie_if.py
@@ -5,5 +5,13 @@ wiek = 25
 Sprawdź czy osoba jest kobietą pełnoletnią (18 lat).
 Jeżeli tak to wyprintuje (energetyki rządzą)
 a jeśli to nieprawda to wyprintuje (wolę jabłka)
+
+Sprawdź dla danych:
+1. imie = Kasia, wiek = 19
+1. imie = Kasia, wiek = 18
+2. imie = Kasia, wiek = 17
+3. imie = Tomek, wiek = 19
+4. imie = Tomek, wiek = 18
+4. imie = Tomek, wiek = 17
 """
 
