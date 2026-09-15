9035b80a7c73243e2781f2b8dca547aee6418c43
quenrythane <artur.babinski.g@gmail.com>
2026-09-15 18:50:09 +0200

    start zadania z funkcji

---
+++ cwiczenia_python/6_funkcje/2_funkcje_zadanie_1.py
@@ -0,0 +1,12 @@
+
+"""
+Stwórz funkcje, która przyjmie 2 argumenty: imie i liczbe
+niech ta funkcja przywita się z osobą po imieniu tyle razy
+ile równać się będzie podana do funkcji liczba
+domyślnie liczba niech równa sie 3
+
+wykonac funkcje dla danych:
+Adam, 2
+Bartek, 4
+Czarek
+"""
