e9638e0b5b1ffe540dfaf85e64eb391316c8ee81
quenrythane <artur.babinski.g@gmail.com>
2026-09-15 18:49:34 +0200

    wprowadzenie do funkcji

---
+++ cwiczenia_python/6_funkcje/1_funkcje_wprowadzenie.py
@@ -0,0 +1,39 @@
+
+"""
+# Ania
+print("Przygotuj kubek Ani")
+print("Przygotuj herbate czarna")
+print("Nalać wody do czajnika")
+print("Zagotuj wodę")
+print("Włóż herbatę do kubka")
+print("Zalej kubek wrzątkiem")
+print("Odstaw na 3 minuty")
+print("Wyjmij torebke z kubka")
+print("Herbata dla Ania jest gotowa!")
+"""
+
+
+# def dodaj(liczba_1, liczba_2):
+#     wynik = liczba_1 + liczba_2
+#     return wynik
+
+
+# print(dodaj(2, 4))
+
+#
+def zrob_herbate(imie="Ania", rodzaj_herbaty="czarna"):
+    print(f"Przygotuj kubek {imie}")
+    print(f"Przygotuj {rodzaj_herbaty} herbata")
+    print("Nalać wody do czajnika")
+    print("Zagotuj wodę")
+    print("Włóż herbatę do kubka")
+    print("Zalej kubek wrzątkiem")
+    print("Odstaw na 3 minuty")
+    print("Wyjmij torebke z kubka")
+    print(f"Herbata dla {imie} jest gotowa!")
+    print()
+
+
+
+zrob_herbate("Tomek")
+
