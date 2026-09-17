bf5645776b285bada91f3bd06ebcfcd8378cca29
quenrythane <artur.babinski.g@gmail.com>
2026-09-17 18:23:04 +0200

    dodanie komentarzy

--- cwiczenia_python/8_pliki_i_moduły/8_1_pliki/1_pliki_wstep.py
+++ cwiczenia_python/8_pliki_i_moduły/8_1_pliki/1_pliki_wstep.py
@@ -1,19 +1,19 @@
 
-# f = open('write_file_name', 'w')
-# f = open('append_file_name', 'a')
-
 
 nazwa_pliku = "plik_xd.txt"
 poziom_uprwanien_read = "r"
+
+""" Otwieramy plik i przechowujemy go w zmiennej o nazwie `plik_xd`.  """
 plik_xd = open(nazwa_pliku, poziom_uprwanien_read)
 
+""" Odczytujemy całą zawartość pliku.  """
 # print(plik_xd.read())
 
-
+""" Odczytujemy linię po linii.  """
 # print("To jest linia nr 1: ", plik_xd.readline())
 # print("To jest linia nr 2: ", plik_xd.readline())
 # print("To jest linia nr 3: ", plik_xd.readline())
 # print("To jest linia nr 4: ", plik_xd.readline())
 
-
+""" Odczytujemy wszystkie linie jako listę.  """
 # print("To jest lista wszystkich linii: ", plik_xd.readlines())
