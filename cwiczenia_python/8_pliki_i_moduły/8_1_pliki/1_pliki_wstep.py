97b38f70b5207c202545aa1e24c95142fd33bbd3
quenrythane <artur.babinski.g@gmail.com>
2026-09-17 18:21:21 +0200

    wstep do czytania plików

---
+++ cwiczenia_python/8_pliki_i_moduły/8_1_pliki/1_pliki_wstep.py
@@ -0,0 +1,19 @@
+
+# f = open('write_file_name', 'w')
+# f = open('append_file_name', 'a')
+
+
+nazwa_pliku = "plik_xd.txt"
+poziom_uprwanien_read = "r"
+plik_xd = open(nazwa_pliku, poziom_uprwanien_read)
+
+# print(plik_xd.read())
+
+
+# print("To jest linia nr 1: ", plik_xd.readline())
+# print("To jest linia nr 2: ", plik_xd.readline())
+# print("To jest linia nr 3: ", plik_xd.readline())
+# print("To jest linia nr 4: ", plik_xd.readline())
+
+
+# print("To jest lista wszystkich linii: ", plik_xd.readlines())
