6684b7b9c998f35d7e4213681f72c88eee80202d
quenrythane <artur.babinski.g@gmail.com>
2026-09-17 20:10:48 +0200

    omówienie funkcji

---
+++ cwiczenia_python/9_programowanie_funkcyjne/1_funkcja_anonimowa.py
@@ -0,0 +1,8 @@
+# def pitagoras(a, b):
+#     return ((a * a) + (b * b)) ** 0.5
+
+# {}.filter(lambda a, b: ((a * a) + (b * b)) ** 0.5)
+
+
+x = [element**2 for element in range(1,6) if element % 2 == 0]
+print(x)
