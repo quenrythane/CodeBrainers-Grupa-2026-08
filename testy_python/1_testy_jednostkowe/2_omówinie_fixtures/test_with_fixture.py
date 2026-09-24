684f5d6cbb964a01b7ddc9275fe4f4667b79edc6
quenrythane <artur.babinski.g@gmail.com>
2026-09-24 19:27:18 +0200

    omowienie fixture i znaczników mark

--- testy_python/1_testy_jednostkowe/2_omówinie_fixtures/test_with_fixture.py
+++ testy_python/1_testy_jednostkowe/2_omówinie_fixtures/test_with_fixture.py
@@ -1,3 +1,5 @@
+import pytest
+
 # pobieramy fixture z pliku conftest.py
 def test_name_employee(employee_fixture):
     assert employee_fixture["name"] == "Artur"
@@ -5,3 +7,11 @@ def test_name_employee(employee_fixture):
 
 def test_age_employee(employee_fixture):
     assert employee_fixture["age"] == 25
+
+
+@pytest.mark.test_imienia
+def test_imie_michal(imie):
+    print(imie)
+    assert imie == "Michal"
+
+# pytest -vsm <nazwa_znacznika>
--- testy_python/1_testy_jednostkowe/conftest.py
+++ testy_python/1_testy_jednostkowe/conftest.py
@@ -10,3 +10,22 @@ def employee_fixture():
         "position": "Junior QA"
     }
     return employee
+
+@pytest.fixture
+def imie():
+    return "Michal"
+
+# @pytest.fixture(scope="session", autouse=True)
+# def start_sesji_testowej():
+#     print("\nRozpoczynam testy")
+#     print("\nprzygotowanie danych")
+#     yield
+#     print("\nKoniec testów")
+#     print("\nczyszczenie po testach")
+
+# @pytest.fixture(scope="function", autouse=True)
+# def start_testu():
+#     print("\nRozpoczynam testy".upper())
+
+
+
