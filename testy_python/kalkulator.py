69dcabfd69e678114eb5872343eeaf2c09e91bf8
quenrythane <artur.babinski.g@gmail.com>
2026-09-24 18:46:31 +0200

    omówienie tdd

--- testy_python/kalkulator.py
+++ testy_python/kalkulator.py
@@ -1,6 +1,7 @@
 
 def dodawanie(a, b):
-    return a + b
+    suma = a + b
+    return suma
 
 def odejmowanie(a, b):
     return a - b
@@ -10,3 +11,7 @@ def mnozenie(a, b):
 
 def dzielenie(a, b):
     return a / b
+
+
+def potegowanie(a, b):
+    return a ** b
--- testy_python/test_kalkulator.py
+++ testy_python/test_kalkulator.py
@@ -8,3 +8,20 @@ from kalkulator import *
 
 def test_dodawanie():
     assert dodawanie(1, 5) == 6
+
+def test_odejmowanie():
+    assert odejmowanie(5, 1) == 6
+
+def test_mnozenie():
+    assert mnozenie(5, 1) == 5
+
+def test_dzielenie():
+    assert dzielenie(5, 1) == 5
+
+def test_potegowanie():
+    assert potegowanie(2, 3) == 8
+    assert potegowanie(10, 2) == 100
+    assert potegowanie(5, 3) == 125
+
+# uruchamianie pojedynczego testu
+# pytest testy_python\test_kalkulator.py::test_mnozenie
