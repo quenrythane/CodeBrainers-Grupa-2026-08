d05578d9b53434f7bd2ded73018cfdbcd8bc33d7
quenrythane <artur.babinski.g@gmail.com>
2026-09-24 20:00:51 +0200

    dodanie konfiguracji i loggera

--- testy_python/1_testy_jednostkowe/3_parametryzacja_testów/test_kalkulator_2.py
+++ testy_python/1_testy_jednostkowe/3_parametryzacja_testów/test_kalkulator_2.py
@@ -1,6 +1,8 @@
 from kalkulator_2 import potegowanie
 import pytest
+import logging
 
+logger = logging.getLogger(__name__)
 
 @pytest.mark.wip
 @pytest.mark.parametrize("podstawa, wykladnik, oczekiwany_wynik", [
@@ -9,7 +11,9 @@ import pytest
     (5, 3, 125)
 ])
 def test_potegowanie(podstawa, wykladnik, oczekiwany_wynik):
+    logger.info(f"Testujemy potęgowanie: {podstawa} do potęgi {wykladnik}")
     assert potegowanie(podstawa, wykladnik) == oczekiwany_wynik
+
     # assert potegowanie(2, 3) == 8
     # assert potegowanie(10, 2) == 100
     # assert potegowanie(5, 3) == 125
