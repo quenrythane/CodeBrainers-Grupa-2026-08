6d441e41fd0184a8fa7185a8b09774a5a033a377
quenrythane <artur.babinski.g@gmail.com>
2026-09-24 20:56:40 +0200

    refactor request by podzielić per linijka potrzebne elementy requestu

--- testy_python/2_testy_api/1_wprowadzenie_do_requests/1_wprowadzenie_do_requests.py
+++ testy_python/2_testy_api/1_wprowadzenie_do_requests/1_wprowadzenie_do_requests.py
@@ -18,6 +18,10 @@ my_payload = {
   "password": "admin"
 }
 
-response = requests.post(login_url, headers=my_headers, json=my_payload)
+response = requests.post(
+    url=login_url,  # endpoint
+    headers=my_headers,  # headers
+    json=my_payload  # body
+)
 
 print(response.json())
