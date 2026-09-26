d1ca99e92d600245d4b1a6e2adb641b979fa40d5
quenrythane <artur.babinski.g@gmail.com>
2026-09-26 09:51:54 +0200

    przeniesienie powtarzalnego kodu do fixtures

--- testy_python/2_testy_api/1_wprowadzenie_do_requests/test_logowanie.py
+++ testy_python/2_testy_api/1_wprowadzenie_do_requests/test_logowanie.py
@@ -2,20 +2,10 @@ import requests
 import pytest
 
 @pytest.mark.api_login
-def test_logowanie():
-  base_url = "http://127.0.0.1:8000/api"
+def test_logowanie(base_url, login_payload):
   login_url = f"{base_url}/login"
 
-  my_headers = {
-    'Content-Type': 'application/json',
-  }
-
-  my_payload = {
-    "username": "admin",
-    "password": "admin"
-  }
-
-  response = requests.post(url=login_url, headers=my_headers, json=my_payload)
+  response = requests.post(url=login_url, json=login_payload)
 
   response_body = response.json()
   assert response.status_code == 200
---
+++ testy_python/2_testy_api/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+@pytest.fixture
+def base_url():
+  return "http://127.0.0.1:8000/api"
+
+
+@pytest.fixture
+def login_payload():
+  return {
+    'username': 'admin',
+    'password': 'admin'
+  }
+
+
+
+
+
+
+@pytest.fixture
+def auth_headers():
+  return {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {access_token}',
+  }
