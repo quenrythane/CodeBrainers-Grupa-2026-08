2616e41ce749eb50fa04f1931df6024400b4583d
quenrythane <artur.babinski.g@gmail.com>
2026-09-26 10:06:56 +0200

    dodanie autentykacji do fixtures

--- testy_python/2_testy_api/1_wprowadzenie_do_requests/test_get_employess.py
+++ testy_python/2_testy_api/1_wprowadzenie_do_requests/test_get_employess.py
@@ -2,30 +2,10 @@ import requests
 import pytest
 
 @pytest.mark.get_employees
-def test_get_employees():
-  base_url = "http://127.0.0.1:8000/api"
-  get_employees_url = f"{base_url}/employees"
-
-
-  # logowanie do api
-  login_url = f"{base_url}/login"
-
-  login_payload = {
-    "username": "admin",
-    "password": "admin"
-  }
-
-  login_response = requests.post(url=login_url, json=login_payload)
-  access_token = login_response.json()['access_token']
-
-
-  # test get employees
-  auth_headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {access_token}',
-  }
-
+def test_get_employees(get_employees_url, auth_headers):
+  # Act
   response = requests.get(url=get_employees_url, headers=auth_headers)
 
+  # Assert
   response_body = response.json()
   assert response.status_code == 200
--- testy_python/2_testy_api/conftest.py
+++ testy_python/2_testy_api/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+import requests
 
+# endpoints urls
 @pytest.fixture
 def base_url():
   return "http://127.0.0.1:8000/api"
@@ -9,6 +11,12 @@ def login_url(base_url):
   return f"{base_url}/login"
 
 @pytest.fixture
+def get_employees_url(base_url):
+  return f"{base_url}/employees"
+
+
+# api login payload
+@pytest.fixture
 def login_payload():
   return {
     'username': 'admin',
@@ -16,13 +24,11 @@ def login_payload():
   }
 
 
-
-
-
-
+# headers for authentication
 @pytest.fixture
-def auth_headers():
-  return {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {access_token}',
-  }
+def auth_headers(login_url, login_payload):
+    login_response = requests.post(url=login_url, json=login_payload)
+    access_token = login_response.json()['access_token']
+    return {
+        'Authorization': f'Bearer {access_token}',
+    }
