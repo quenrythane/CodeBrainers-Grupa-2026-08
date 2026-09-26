c1b164bcbbfd3dcfeb2d85c26ec1640e577d6357
quenrythane <artur.babinski.g@gmail.com>
2026-09-26 12:10:44 +0200

    dodanie logów do testu api

--- testy_python/2_testy_api/2_wykonanie_testów_api/test_create_employee.py
+++ testy_python/2_testy_api/2_wykonanie_testów_api/test_create_employee.py
@@ -9,6 +9,10 @@ logger = logging.getLogger(__name__)
 @pytest.mark.create_employee
 def test_create_employee(employees_url, auth_headers, create_employee_correct_payload):
   # Act
+  logger.debug(f"URL: {employees_url}")
+  logger.debug(f"Auth headers: {auth_headers}")
+  logger.debug(f"Payload: {create_employee_correct_payload}")
+  logger.info("Wysyłanie zapytania POST...")
   response = requests.post(
     url=employees_url,
     headers=auth_headers,
@@ -17,8 +21,14 @@ def test_create_employee(employees_url, auth_headers, create_employee_correct_pa
 
   # Assert
   response_body = response.json()
+  logger.debug(f"Response: {response_body}")
+
   assert response.status_code == 200
   for key, value in create_employee_correct_payload.items():
+    logger.info(f"Sprawdzam klucz body_response: {key.upper()} o wartości: {str(response_body[key]).upper()}, który powinien mieć wartość {str(value).upper()}")
     assert response_body[key] == value
   assert 'id' in response_body
+  logger.info(f"Nowo utworzony pracownik ma ID: {response_body['id']}")
   assert isinstance(response_body['id'], int)
+  logger.info(f"Typ ID jest typu {type(response_body['id'])}")
+
